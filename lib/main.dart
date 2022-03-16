215f754a2fd9f06e75545f73ab5c1425cb6afd98
Doron Tal <doron@tracktunes.org>
2022-03-16 19:12:42 -0400

    minor

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,12 @@ void main() {
   runApp(const MyApp());
 }
 
-// 1. extend from ConsumerWidget
 class MyApp extends StatelessWidget {
   const MyApp({
     Key? key,
   }) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    // 3. watch selectedPageBuilderProvider
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
