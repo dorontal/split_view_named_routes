9472d30fbeb58673bb93e0c98bd8ef2615cc7c50
Doron Tal <doron@tracktunes.org>
2022-03-17 23:53:15 -0400

    minor

--- lib/widgets/split_view.dart
+++ lib/widgets/split_view.dart
@@ -19,7 +19,7 @@ IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-// Minor modifications by Doron Tal
+// Minor changes by Doron Tal
 
 import 'package:flutter/material.dart';
 
