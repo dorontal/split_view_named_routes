2fb105d89c6e985ba8f69ea6a2112511b23199ed
Doron Tal <doron@tracktunes.org>
2022-03-19 23:47:03 -0400

    minor

--- lib/constants/menu_pages.dart
+++ lib/constants/menu_pages.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../screens/first.dart';
+import '../screens/last.dart';
 import '../screens/second.dart';
 import '../widgets/menu.dart';
 
@@ -15,3 +16,11 @@ final List<MenuPage> menuPages = [
       menuIcon: Icons.table_bar,
       menuText: 'Second page')
 ];
+
+final List<MenuPage> menuPages2 = [
+  MenuPage(
+      url: 'last-page',
+      page: const Last(),
+      menuIcon: Icons.home,
+      menuText: 'Last page')
+}
