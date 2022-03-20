8a986bc08eb741e2bb89286506f965a9b420335f
Doron Tal <doron@tracktunes.org>
2022-03-19 21:17:14 -0400

    minor

--- lib/services/app_router.dart
+++ lib/services/app_router.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import '../about.dart';
 import '../widgets/split_view.dart';
 import '../widgets/app_menu.dart';
+
+const headerUrl = 'header';
 
 class MenuPage {
   final Widget page;
@@ -19,6 +22,7 @@ class AppRouter {
   final List<MenuPage> menuPages;
 
   final Map<String, MenuPage> _itemsMap = {};
+  final _header = const About();
 
   AppRouter({required this.menuPages}) {
     for (var i = 0; i < menuPages.length; i++) {
@@ -28,15 +32,18 @@ class AppRouter {
   }
 
   void addRoute(String url, MenuPage menuPage) {
+    assert(url != headerUrl);
     _itemsMap[url] = menuPage;
   }
 
   Route<dynamic> generateRoute(RouteSettings settings) {
-    final MenuPage menuPage =
-        settings.name == '/' ? menuPages[0] : _itemsMap[settings.name]!;
+    final url = settings.name;
+    final Widget page = url == headerUrl
+        ? _header
+        : (url == '/' ? menuPages[0].page : _itemsMap[settings.name]!.page);
     return MaterialPageRoute(
         settings: settings,
-        builder: (context) => SplitView(
-            menu: AppMenu(menuPages: menuPages), content: menuPage.page));
+        builder: (context) =>
+            SplitView(menu: AppMenu(menuPages: menuPages), content: page));
   }
 }
--- lib/widgets/tracktunes_logo.dart
+++ lib/widgets/tracktunes_logo.dart
@@ -25,7 +25,8 @@ class TracktunesLogo extends StatelessWidget {
     }
 
     return Padding(
-        padding: const EdgeInsets.fromLTRB(0, 16, 0, 12),
+        // padding: const EdgeInsets.fromLTRB(0, 16, 0, 12),
+        padding: const EdgeInsets.all(0),
         child: Column(
             mainAxisAlignment: MainAxisAlignment.center, children: children));
   }
