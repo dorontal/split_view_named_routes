465171384ded54899b6e8ea0f57d0699d480dd26
Doron Tal <doron@tracktunes.org>
2022-03-19 21:00:30 -0400

    minor

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 import '../widgets/split_view_scaffold.dart';
 import '../widgets/tracktunes_logo.dart';
 
-class Header extends StatelessWidget {
-  const Header({Key? key}) : super(key: key);
+class About extends StatelessWidget {
+  const About({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) => const SplitViewScaffold(
-      title: 'Header', body: Center(child: TracktunesLogo(showText: true)));
+      title: 'About', body: Center(child: TracktunesLogo(showText: true)));
 }
