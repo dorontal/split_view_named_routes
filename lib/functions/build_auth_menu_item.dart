d604903166eabef4ff6e8a0247f734bab0e0ba53
Doron Tal <doron@tracktunes.org>
2022-03-20 17:57:42 -0400

    minor

--- lib/functions/build_auth_menu_item.dart
+++ lib/functions/build_auth_menu_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../providers/signed_in.dart';
 import '../widgets/ok_alert.dart';
 
@@ -11,13 +12,11 @@ Widget buildAuthMenuItem(BuildContext context, WidgetRef ref) {
       color: Colors.transparent,
       child: ListTile(
           leading: Icon(signedIn ? Icons.logout : Icons.login),
-          title: Text(signedIn ? 'Sign Out' : 'Log In (or Sign Up)'),
+          title: Text(signedIn ? 'Log Out' : 'Log In (or Sign Up)'),
           onTap: () async {
             final newState = !signedIn;
             signedInProvider.state = newState;
-            await showOKAlert(context,
-                message: newState
-                    ? 'You are now logged in!'
-                    : 'You are now signed out!');
+            final inOrOut = newState ? 'in' : 'out';
+            await showOKAlert(context, message: 'You are now logged $inOrOut!');
           }));
 }
