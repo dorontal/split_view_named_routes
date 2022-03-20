fb13c666e69ab813c7fe05b254e996ad4ee1f599
Doron Tal <doron@tracktunes.org>
2022-03-19 22:26:15 -0400

    minor

--- lib/services/app_router.dart
+++ lib/services/app_router.dart
@@ -9,9 +9,17 @@ class MenuPage {
   final String url;
   final IconData? menuIcon;
   final String? menuText;
+  final Function? menuAction;
 
-  MenuPage(
-      {required this.page, required this.url, this.menuIcon, this.menuText});
+  // you cannot have both 'action' and ('page' or 'url') not null and they
+  // cannot all be null
+  MenuPage({
+    required this.page,
+    required this.url,
+    this.menuIcon,
+    this.menuText,
+    this.menuAction,
+  });
 }
 
 class AppRouter {
