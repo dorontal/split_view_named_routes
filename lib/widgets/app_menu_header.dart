a88cad624ae67a8e50925b43dbb4f8fa6e47b74c
Doron Tal <doron@tracktunes.org>
2022-03-20 00:51:50 -0400

    minor

--- lib/widgets/app_menu_header.dart
+++ lib/widgets/app_menu_header.dart
@@ -3,9 +3,7 @@ import '../constants/header_url.dart';
 import './tracktunes_logo.dart';
 
 class AppMenuHeader extends StatelessWidget {
-  const AppMenuHeader({
-    Key? key,
-  }) : super(key: key);
+  const AppMenuHeader({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
